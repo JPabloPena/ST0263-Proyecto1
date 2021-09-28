d4edb6f4b4b9b2202c0e151c78f7e8d14af334df
jssaninv <jssaninv@eafit.edu.co>
2021-09-27 23:42:48 -0500

    modificaciones para el hash

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from os import pipe
 import sys
 from socket import socket, error
 from threading import Thread
@@ -7,7 +8,7 @@ class Client(Thread):
 
     #Método para indentificar cada cliente que se conecta al servidor
     def __init__(self, conn, addr):
-
+        self.table = [None] * 127
         Thread.__init__(self) #Crea el hilo para el cliente
 
         self.conn = conn #Guarda la conexión
@@ -33,8 +34,15 @@ class Client(Thread):
                 file = open('server_files/' + dataArray[1], "w")# w -> write
                 file.write(dataArray[2])
                 file.close()
+                #H= Client()
+               # H.Insert("hoa.txt")
+                
+                #key= add(self,dataArray[1],dataArray[1])
+                key=getHash(dataArray[1])
+               # print(key)
+                #print(get(self,dataArray[1]))
                 node(key, dataArray[1])
-                key = key + 1
+                #key = key + 1
                 msg = ' [x] El archivo se guardo correctamente!'
                 self.conn.send(msg.encode())
 
@@ -53,17 +61,66 @@ class Client(Thread):
         self.conn.close() #Termina la conexión con el cliente
 
     
+def Hash_func(self, value):
+        key = 0
+        for i in range(0,len(value)):
+            key += ord(value[i])
+        return key % 127
+
+def Insert(self, value): # Metodo para ingresar elementos
+    hash = self.Hash_func(value)
+    if self.table[hash] is None:
+        self.table[hash] = value
+   
+def Search(self,value): # Metodo para buscar elementos
+        hash = self.Hash_func(value)
+        if self.table[hash] is None:
+            return None
+        else:
+            return hex(id(self.table[hash]))
+  
+def Remove(self,value): # Metodo para eleminar elementos
+        hash = self.Hash_func(value)
+        if self.table[hash] is None:
+            print("No hay elementos con ese valor", value)
+        else:
+            print("Elemento con valor", value, "eliminado")
+            self.table[hash] is None
+        
+
+
+def getHash(key):
+    h = 0
+    for char in key:
+        h += ord(char)
+    #print (h)
+    return h % 100
+
+def add(self,key,val):
+    h=self.getHash(key)
+    self.arr[h]=val
+
+def get(self, key):
+    h=self.getHash(key)
+    return self.arr[h]
+
+
+
+node1 = []
+node2 = []
+node3 = []
 def node(key, value):
     hash = {}
-    node1 = []
-    node2 = []
-    node3 = []
-
+    count = 0
     hash[key] = value
     node1.append(hash)
-
+    print(node1[0])
     for n in node1:
-        print(n)
+        if(n!=NULL):
+          count= count+1
+          print(n)
+    print(count)
+
 
 
 def Main():
